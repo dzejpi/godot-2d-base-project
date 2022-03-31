63eb52e0f338a06159a6415f3ae5c7d44626b09d
dzejpi <dzejjpii@gmail.com>
2022-04-01 00:24:55 +0200

    Added the basic controls for music and sound buttons.

--- scripts/MusicButton.gd
+++ scripts/MusicButton.gd
@@ -1,7 +1,8 @@
 extends TextureButton
 
 
-# var a = 2
+var music_on = true
+onready var music_label = $"MusicLabel"
 
 
 func _ready():
@@ -9,8 +10,19 @@ func _ready():
 
 
 func _process(delta):
-	pass
+	if music_on:
+		pass
+	else:
+		pass
 
 
 func _on_MusicButton_pressed():
-	pass
+	if music_on:
+		music_on = false
+		music_label.text = "Music: Off"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+		release_focus()
+	else:
+		music_on = true
+		music_label.text = "Music: On"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
--- scripts/SoundButton.gd
+++ scripts/SoundButton.gd
@@ -1,7 +1,8 @@
 extends TextureButton
 
 
-# var a = 2
+var sound_on = true
+onready var sound_label = $"SoundLabel"
 
 
 func _ready():
@@ -9,8 +10,19 @@ func _ready():
 
 
 func _process(delta):
-	pass
+	if sound_on:
+		pass
+	else:
+		pass
 
 
 func _on_SoundButton_pressed():
-	pass
+	if sound_on:
+		sound_on = false
+		sound_label.text = "Sound: Off"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), true)
+		release_focus()
+	else:
+		sound_on = true
+		sound_label.text = "Sound: On"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), false)
