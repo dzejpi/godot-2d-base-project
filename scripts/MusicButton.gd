69210f249297bc907923e864557e0b0d0f8c3794
dzejpi <dzejjpii@gmail.com>
2022-04-01 00:37:06 +0200

    Music and Sound buttons release focus after every actions, properly detect the audio bus state and switch the state accordingly.

--- scripts/MusicButton.gd
+++ scripts/MusicButton.gd
@@ -6,7 +6,18 @@ onready var music_label = $"MusicLabel"
 
 
 func _ready():
-	pass
+	if AudioServer.is_bus_mute(AudioServer.get_bus_index("Music")):
+		music_on = false
+		music_label.text = "Music: Off"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+		self.pressed = false
+		release_focus()
+	else:
+		music_on = true
+		music_label.text = "Music: On"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
+		self.pressed = true
+		release_focus()
 
 
 func _process(delta):
@@ -21,8 +32,11 @@ func _on_MusicButton_pressed():
 		music_on = false
 		music_label.text = "Music: Off"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+		self.pressed = false
 		release_focus()
 	else:
 		music_on = true
 		music_label.text = "Music: On"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
+		self.pressed = true
+		release_focus()
--- scripts/SoundButton.gd
+++ scripts/SoundButton.gd
@@ -6,7 +6,18 @@ onready var sound_label = $"SoundLabel"
 
 
 func _ready():
-	pass
+	if AudioServer.is_bus_mute(AudioServer.get_bus_index("Sound")):
+		sound_on = false
+		sound_label.text = "Sound: Off"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), true)
+		self.pressed = false
+		release_focus()
+	else:
+		sound_on = true
+		sound_label.text = "Sound: On"
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), false)
+		self.pressed = true
+		release_focus()
 
 
 func _process(delta):
@@ -21,8 +32,11 @@ func _on_SoundButton_pressed():
 		sound_on = false
 		sound_label.text = "Sound: Off"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), true)
+		self.pressed = false
 		release_focus()
 	else:
 		sound_on = true
 		sound_label.text = "Sound: On"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sound"), false)
+		self.pressed = true
+		release_focus()
